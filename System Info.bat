0cc8efdaa4a0f3afec5439fe07d4496a04adb0f4
ZODO <99730271+ZaxelGaming@users.noreply.github.com>
2022-09-17 02:15:16 +0200

    Add files via upload

---
+++ System Info.bat
@@ -0,0 +1,17 @@
+echo off
+color 0c
+@echo off 
+cls
+Title Temp Deleter by ZODO
+
+@echo.  ________  ____   ___
+@echo. ^|^_^_  ^/ ^_ ^\^|  ^_ ^\ ^/ ^_ ^\
+@echo.   ^/ ^/ ^| ^| ^| ^| ^| ^| ^| ^| ^|
+@echo.  ^/ ^/^| ^|^_^| ^| ^|^_^| ^| ^|^_^| ^|
+@echo. ^/^_^_^_^_^\^_^_^_^/^|^_^_^_^_^/ ^\^_^_^_^/                        
+ECHO.
+
+systeminfo
+wmic baseboard get product,Manufacturer,version,serialnumber
+ipconfig
+pause
